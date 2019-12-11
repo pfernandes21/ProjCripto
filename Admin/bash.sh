ce97a4fd69f05bbeac3d79f2901a44fc8403c6a1
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2019-12-11 17:32:30 +0000

    bas

--- Admin/bash.sh
+++ Admin/bash.sh
@@ -1,68 +1,55 @@
-#Create CA certificate and send to Tally app
-rm -r Admin/CA
-mkdir Admin/CA
-chmod 0770 CA;
-echo "Create CA Certificates"
-openssl genrsa -des3 -out Admin/CA/my-ca.key 2048;
-openssl req -new -x509 -days 3650 -key Admin/CA/my-ca.key -out Admin/CA/my-ca.crt;
-cp Admin/CA/my-ca.crt TallyApp/Certs/;
+cd Admin/;
 #
-#Create certificate for voter0
-# openssl genrsa -des3 -out client-cert0.key 1024;
-# openssl rsa -in client-cert0.key -out clientPrivateKey0.key;
-# openssl req -new -key client-cert0.key -out client-cert0.csr;
-# openssl x509 -req -in client-cert0.csr -out client-cert0.crt -sha1 -CA CA/my-ca.crt -CAkey CA/my-ca.key -CAcreateserial -days 3650;
-# openssl x509 -pubkey -noout -in client-cert0.crt > clientPublicKey0.key;
-# openssl pkcs12 -export -in client-cert0.crt -inkey client-cert0.key -name 'User Cert' -out client-cert0.p12;
-# openssl pkcs12 -in client-cert0.p12 -clcerts -nokeys -info;
-# chmod 444 client-cert0.p12;
-# mv *.key Certs;
-# mv *.p12 Certs;
-# mv *.csr Certs;
-# mv *.crt Certs;
-# #
-# #Create certificate for voter1
-# openssl genrsa -des3 -out client-cert1.key 1024;
-# openssl rsa -in client-cert1.key -out clientPrivateKey1.key;
-# openssl req -new -key client-cert1.key -out client-cert1.csr;
-# openssl x509 -req -in client-cert1.csr -out client-cert1.crt -sha1 -CA CA/my-ca.crt -CAkey CA/my-ca.key -CAcreateserial -days 3650;
-# openssl x509 -pubkey -noout -in client-cert1.crt > clientPublicKey1.key;
-# openssl pkcs12 -export -in client-cert1.crt -inkey client-cert1.key -name 'User Cert' -out client-cert1.p12;
-# openssl pkcs12 -in client-cert1.p12 -clcerts -nokeys -info;
-# chmod 444 client-cert1.p12;
-# mv *.key Certs;
-# mv *.p12 Certs;
-# mv *.csr Certs;
-# mv *.crt Certs;
-# #
-# #Send root CA cert to Voter0
-# cp CA/my-ca.crt ../VoterApp/client0/;
-# #Send voter0 private key and certificate
-# cp Certs/client-cert0.crt ../VoterApp/client0/;
-# cp Certs/client-cert0.key ../VoterApp/client0/;
-# cp Certs/clientPrivateKey0.key ../VoterApp/client0/;
-# cp Certs/clientPublicKey0.key ../VoterApp/client0/;
-# #Send Election public key to Voter0
-# cp electionKeys/publicKey.txt ../VoterApp/client0/electionPublicKey.txt;
-# #
-# #Send root CA cert to Voter0
-# cp CA/my-ca.crt ../VoterApp/client0/;
-# #Send voter0 private key and certificate
-# cp Certs/client-cert0.crt ../VoterApp/client0/;
-# cp Certs/client-cert0.key ../VoterApp/client0/;
-# cp Certs/clientPrivateKey0.key ../VoterApp/client0/;
-# cp Certs/clientPublicKey0.key ../VoterApp/client0/;
-# #Send Election public key to Voter0
-# cp electionKeys/publicKey.txt ../VoterApp/client0/electionPublicKey.txt;
-# #
-# #Send root CA cert to Voter1
-# cp CA/my-ca.crt ../VoterApp/client1/;
-# #Send voter1 private key and certificate
-# cp Certs/client-cert1.crt ../VoterApp/client1/;
-# cp Certs/client-cert1.key ../VoterApp/client1/;
-# cp Certs/clientPrivateKey1.key ../VoterApp/client1/;
-# cp Certs/clientPublicKey1.key ../VoterApp/client1/;
-# #Send Election public key to Voter1
-# cp electionKeys/publicKey.txt ../VoterApp/client1/electionPublicKey.txt;
-# #
+#Create Directories
+rm -r CA;
+mkdir CA;
+chmod 0770 CA;
+rm -r Certs;
+mkdir Certs;
+#
+#Create CA certificate and send to Tally
+echo "*****Create CA Certificate*****";
+openssl genrsa -des3 -out CA/my-ca.key 2048;
+openssl req -new -x509 -days 3650 -key CA/my-ca.key -out CA/my-ca.crt;
+cp CA/my-ca.crt TallyApp/Certs/;
+#
+#Create certificate for voters
+for (( i=1; i<=${1}; i++ ))
+do
+    echo "Create Certificate for Voter${i}";
+    openssl genrsa -des3 -out client-cert${i}.key 1024;
+    openssl rsa -in client-cert${i}.key -out clientPrivateKey${i}.key;
+    openssl req -new -key client-cert${i}.key -out client-cert${i}.csr;
+    openssl x509 -req -in client-cert${i}.csr -out client-cert${i}.crt -sha1 -CA CA/my-ca.crt -CAkey CA/my-ca.key -CAcreateserial -days 3650;
+    openssl x509 -pubkey -noout -in client-cert${i}.crt > clientPublicKey${i}.key;
+    openssl pkcs12 -export -in client-cert${i}.crt -inkey client-cert${i}.key -name 'User Cert' -out client-cert${i}.p12;
+    openssl pkcs12 -in client-cert${i}.p12 -clcerts -nokeys -info;
+    chmod 444 client-cert${i}.p12;
+done
+#Move to Certificates Directory
+mv *.key Certs;
+mv *.p12 Certs;
+mv *.csr Certs;
+mv *.crt Certs;
+#
+#Create Voters Directories
+for (( i=1; i<=${1}; i++ ))
+do
+    rm -r VoterApp/Voter${i};
+    mkdir VoterApp/Voter${i};
+done
+#Send keys and files to Voters
+for (( i=1; i<=${1}; i++ ))
+do
+    #Send root CA cert to Voter
+    cp CA/my-ca.crt ../VoterApp/client${i}/;
+    #Send voter private key and certificate
+    cp Certs/client-cert${i}.crt ../VoterApp/client${i}/;
+    cp Certs/client-cert${i}.key ../VoterApp/client${i}/;
+    cp Certs/clientPrivateKey${i}.key ../VoterApp/client${i}/;
+    cp Certs/clientPublicKey${i}.key ../VoterApp/client${i}/;
+    #Send Election public key to Voter
+    cp electionKeys/publicKey.txt ../VoterApp/client${i}/electionPublicKey.txt;
+done
+#
 # #encryptar private key com password
