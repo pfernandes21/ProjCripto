914bb75b4fbd88f52b8f1e17c8c935bed4331f65
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2019-12-13 02:16:22 +0000

    a

--- Signer.cpp
+++ Signer.cpp
@@ -19,32 +19,31 @@
 using namespace std;
 using namespace seal;
 
-
 RSA *createPrivateRSA(std::string key)
 {
-  RSA *rsa = NULL;
-  const char *c_string = key.c_str();
-  BIO *keybio = BIO_new_mem_buf((void *)c_string, -1);
-  if (keybio == NULL)
-  {
-    return 0;
-  }
-  rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
-  return rsa;
+    RSA *rsa = NULL;
+    const char *c_string = key.c_str();
+    BIO *keybio = BIO_new_mem_buf((void *)c_string, -1);
+    if (keybio == NULL)
+    {
+        return 0;
+    }
+    rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+    return rsa;
 }
 
 RSA *createPublicRSA(std::string key)
 {
-  RSA *rsa = NULL;
-  BIO *keybio;
-  const char *c_string = key.c_str();
-  keybio = BIO_new_mem_buf((void *)c_string, -1);
-  if (keybio == NULL)
-  {
-    return 0;
-  }
-  rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
-  return rsa;
+    RSA *rsa = NULL;
+    BIO *keybio;
+    const char *c_string = key.c_str();
+    keybio = BIO_new_mem_buf((void *)c_string, -1);
+    if (keybio == NULL)
+    {
+        return 0;
+    }
+    rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
+    return rsa;
 }
 
 bool RSASign(RSA *rsa,
@@ -53,28 +52,28 @@ bool RSASign(RSA *rsa,
              unsigned char **EncMsg,
              size_t *MsgLenEnc)
 {
-  EVP_MD_CTX *m_RSASignCtx = EVP_MD_CTX_new();
-  EVP_PKEY *priKey = EVP_PKEY_new();
-  EVP_PKEY_assign_RSA(priKey, rsa);
-  if (EVP_DigestSignInit(m_RSASignCtx, NULL, EVP_sha256(), NULL, priKey) <= 0)
-  {
-    return false;
-  }
-  if (EVP_DigestSignUpdate(m_RSASignCtx, Msg, MsgLen) <= 0)
-  {
-    return false;
-  }
-  if (EVP_DigestSignFinal(m_RSASignCtx, NULL, MsgLenEnc) <= 0)
-  {
-    return false;
-  }
-  *EncMsg = (unsigned char *)malloc(*MsgLenEnc);
-  if (EVP_DigestSignFinal(m_RSASignCtx, *EncMsg, MsgLenEnc) <= 0)
-  {
-    return false;
-  }
-  EVP_MD_CTX_free(m_RSASignCtx);
-  return true;
+    EVP_MD_CTX *m_RSASignCtx = EVP_MD_CTX_new();
+    EVP_PKEY *priKey = EVP_PKEY_new();
+    EVP_PKEY_assign_RSA(priKey, rsa);
+    if (EVP_DigestSignInit(m_RSASignCtx, NULL, EVP_sha256(), NULL, priKey) <= 0)
+    {
+        return false;
+    }
+    if (EVP_DigestSignUpdate(m_RSASignCtx, Msg, MsgLen) <= 0)
+    {
+        return false;
+    }
+    if (EVP_DigestSignFinal(m_RSASignCtx, NULL, MsgLenEnc) <= 0)
+    {
+        return false;
+    }
+    *EncMsg = (unsigned char *)malloc(*MsgLenEnc);
+    if (EVP_DigestSignFinal(m_RSASignCtx, *EncMsg, MsgLenEnc) <= 0)
+    {
+        return false;
+    }
+    EVP_MD_CTX_free(m_RSASignCtx);
+    return true;
 }
 
 bool RSAVerifySignature(RSA *rsa,
@@ -84,194 +83,135 @@ bool RSAVerifySignature(RSA *rsa,
                         size_t MsgLen,
                         bool *Authentic)
 {
-  *Authentic = false;
-  EVP_PKEY *pubKey = EVP_PKEY_new();
-  EVP_PKEY_assign_RSA(pubKey, rsa);
-  EVP_MD_CTX *m_RSAVerifyCtx = EVP_MD_CTX_new();
+    *Authentic = false;
+    EVP_PKEY *pubKey = EVP_PKEY_new();
+    EVP_PKEY_assign_RSA(pubKey, rsa);
+    EVP_MD_CTX *m_RSAVerifyCtx = EVP_MD_CTX_new();
 
-  if (EVP_DigestVerifyInit(m_RSAVerifyCtx, NULL, EVP_sha256(), NULL, pubKey) <= 0)
-  {
-    return false;
-  }
-  if (EVP_DigestVerifyUpdate(m_RSAVerifyCtx, Msg, MsgLen) <= 0)
-  {
-    return false;
-  }
-  int AuthStatus = EVP_DigestVerifyFinal(m_RSAVerifyCtx, MsgHash, MsgHashLen);
-  if (AuthStatus == 1)
-  {
-    *Authentic = true;
-    EVP_MD_CTX_free(m_RSAVerifyCtx);
-    return true;
-  }
-  else if (AuthStatus == 0)
-  {
-    *Authentic = false;
-    EVP_MD_CTX_free(m_RSAVerifyCtx);
-    return true;
-  }
-  else
-  {
-    *Authentic = false;
-    EVP_MD_CTX_free(m_RSAVerifyCtx);
-    return false;
-  }
+    if (EVP_DigestVerifyInit(m_RSAVerifyCtx, NULL, EVP_sha256(), NULL, pubKey) <= 0)
+    {
+        return false;
+    }
+    if (EVP_DigestVerifyUpdate(m_RSAVerifyCtx, Msg, MsgLen) <= 0)
+    {
+        return false;
+    }
+    int AuthStatus = EVP_DigestVerifyFinal(m_RSAVerifyCtx, MsgHash, MsgHashLen);
+    if (AuthStatus == 1)
+    {
+        *Authentic = true;
+        EVP_MD_CTX_free(m_RSAVerifyCtx);
+        return true;
+    }
+    else if (AuthStatus == 0)
+    {
+        *Authentic = false;
+        EVP_MD_CTX_free(m_RSAVerifyCtx);
+        return true;
+    }
+    else
+    {
+        *Authentic = false;
+        EVP_MD_CTX_free(m_RSAVerifyCtx);
+        return false;
+    }
 }
 
 void Base64Encode(const unsigned char *buffer,
                   size_t length,
                   char **base64Text)
 {
-  BIO *bio, *b64;
-  BUF_MEM *bufferPtr;
+    BIO *bio, *b64;
+    BUF_MEM *bufferPtr;
 
-  b64 = BIO_new(BIO_f_base64());
-  bio = BIO_new(BIO_s_mem());
-  bio = BIO_push(b64, bio);
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new(BIO_s_mem());
+    bio = BIO_push(b64, bio);
 
-  BIO_write(bio, buffer, length);
-  BIO_flush(bio);
-  BIO_get_mem_ptr(bio, &bufferPtr);
-  BIO_set_close(bio, BIO_NOCLOSE);
-  BIO_free_all(bio);
+    BIO_write(bio, buffer, length);
+    BIO_flush(bio);
+    BIO_get_mem_ptr(bio, &bufferPtr);
+    BIO_set_close(bio, BIO_NOCLOSE);
+    BIO_free_all(bio);
 
-  *base64Text = (*bufferPtr).data;
+    *base64Text = (*bufferPtr).data;
 }
 
 size_t calcDecodeLength(const char *b64input)
 {
-  size_t len = strlen(b64input), padding = 0;
+    size_t len = strlen(b64input), padding = 0;
 
-  if (b64input[len - 1] == '=' && b64input[len - 2] == '=') //last two chars are =
-    padding = 2;
-  else if (b64input[len - 1] == '=') //last char is =
-    padding = 1;
-  return (len * 3) / 4 - padding;
+    if (b64input[len - 1] == '=' && b64input[len - 2] == '=') //last two chars are =
+        padding = 2;
+    else if (b64input[len - 1] == '=') //last char is =
+        padding = 1;
+    return (len * 3) / 4 - padding;
 }
 
 void Base64Decode(const char *b64message, unsigned char **buffer, size_t *length)
 {
-  BIO *bio, *b64;
+    BIO *bio, *b64;
 
-  int decodeLen = calcDecodeLength(b64message);
-  *buffer = (unsigned char *)malloc(decodeLen + 1);
-  (*buffer)[decodeLen] = '\0';
+    int decodeLen = calcDecodeLength(b64message);
+    *buffer = (unsigned char *)malloc(decodeLen + 1);
+    (*buffer)[decodeLen] = '\0';
 
-  bio = BIO_new_mem_buf(b64message, -1);
-  b64 = BIO_new(BIO_f_base64());
-  bio = BIO_push(b64, bio);
+    bio = BIO_new_mem_buf(b64message, -1);
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_push(b64, bio);
 
-  *length = BIO_read(bio, *buffer, strlen(b64message));
-  BIO_free_all(bio);
+    *length = BIO_read(bio, *buffer, strlen(b64message));
+    BIO_free_all(bio);
 }
 
 char *signMessage(std::string privateKey, std::string plainText)
 {
-  RSA *privateRSA = createPrivateRSA(privateKey);
-  unsigned char *encMessage;
-  char *base64Text;
-  size_t encMessageLength;
-  RSASign(privateRSA, (unsigned char *)plainText.c_str(), plainText.length(), &encMessage, &encMessageLength);
-  Base64Encode(encMessage, encMessageLength, &base64Text);
-  free(encMessage);
-  return base64Text;
+    RSA *privateRSA = createPrivateRSA(privateKey);
+    unsigned char *encMessage;
+    char *base64Text;
+    size_t encMessageLength;
+    RSASign(privateRSA, (unsigned char *)plainText.c_str(), plainText.length(), &encMessage, &encMessageLength);
+    Base64Encode(encMessage, encMessageLength, &base64Text);
+    free(encMessage);
+    return base64Text;
 }
 
 bool verifySignature(std::string publicKey, std::string plainText, char *signatureBase64)
 {
-  RSA *publicRSA = createPublicRSA(publicKey);
-  unsigned char *encMessage;
-  size_t encMessageLength;
-  bool authentic;
-  Base64Decode(signatureBase64, &encMessage, &encMessageLength);
-  bool result = RSAVerifySignature(publicRSA, encMessage, encMessageLength, plainText.c_str(), plainText.length(), &authentic);
-  return result & authentic;
+    RSA *publicRSA = createPublicRSA(publicKey);
+    unsigned char *encMessage;
+    size_t encMessageLength;
+    bool authentic;
+    Base64Decode(signatureBase64, &encMessage, &encMessageLength);
+    bool result = RSAVerifySignature(publicRSA, encMessage, encMessageLength, plainText.c_str(), plainText.length(), &authentic);
+    return result & authentic;
 }
 
 bool simpleSHA256(void *input, unsigned long length, unsigned char *md)
 {
-  SHA256_CTX context;
-  if (!SHA256_Init(&context))
-    return false;
+    SHA256_CTX context;
+    if (!SHA256_Init(&context))
+        return false;
 
-  if (!SHA256_Update(&context, (unsigned char *)input, length))
-    return false;
+    if (!SHA256_Update(&context, (unsigned char *)input, length))
+        return false;
 
-  if (!SHA256_Final(md, &context))
-    return false;
+    if (!SHA256_Final(md, &context))
+        return false;
 
-  return true;
+    return true;
 }
 
 int main()
 {
-    /* Load the human readable error strings for libcrypto */
-	ERR_load_crypto_strings();
 
-	/* Load all digest and cipher algorithms */
-	OpenSSL_add_all_algorithms();
-
-	/* Load config file, and other important initialisation */
-	OPENSSL_config(NULL);
-
-    int myId = 0, myVote = 0;
-
-	string line;
-
-	cout << "Insira o seu numero de votante:" << endl;
-	cin >> myId;
-
-	//get id do voto no ficheiro
-	ifstream IdFileIn("Voter/id.txt", ios::in);
-	if (IdFileIn.is_open())
-	{
-		while (getline(IdFileIn, line))
-		{
-			myVote = stoi(line);
-		}
-		IdFileIn.close();
-	}
-	else
-	{
-		return 0;
-	}
-
-    string filename;
-
-    filename = "Voter/Voter" + to_string(myId) + "/clientPrivateKey" + to_string(myId) + ".key";
-    std::ifstream privateKeyFile(filename);
-	std::string myprivateKey((std::istreambuf_iterator<char>(privateKeyFile)),
-							 std::istreambuf_iterator<char>());
-
-	cout << myprivateKey;
-
-    //Make signature from tempFile
-	std::ifstream tempFile("signatureTemp.txt");
-	std::string dataTempFile((std::istreambuf_iterator<char>(tempFile)),
-							 std::istreambuf_iterator<char>());
-
-    filename = "Voter/vote" + to_string(myVote) + ".txt";
-	ofstream votesFile(filename);
-
+    std::ifstream privateKeyFile("clientPrivateKey.key");
+    std::string myprivateKey((std::istreambuf_iterator<char>(privateKeyFile)), std::istreambuf_iterator<char>());
+    std::ifstream tempFile("signatureTemp.txt");
+    std::string dataTempFile((std::istreambuf_iterator<char>(tempFile)), std::istreambuf_iterator<char>());
+    
     char *signature = signMessage(myprivateKey, dataTempFile);
-	votesFile << ' ' << signature;
-	votesFile.close();
 
-	filename = "Voter/vote" + to_string(myVote) + ".txt";
-
-    char command[50];
-	sprintf(command, "mv %s Ballot/", filename.c_str());
-	system(command);
-	system("rm signatureTemp.txt");
-
-	/* Removes all digests and ciphers */
-	EVP_cleanup();
-
-	/* if you omit the next, a small leak may be left when you make use of the BIO (low level API) for e.g. base64 transformations */
-	CRYPTO_cleanup_all_ex_data();
-
-	/* Remove error strings */
-	ERR_free_strings();
-
+    cout << signature;
     return 0;
 }
